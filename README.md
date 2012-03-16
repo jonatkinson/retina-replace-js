60c688cd83a4573c2697bd54ea85fd6f62ea4d2a
Leon Smith <leonsmith000@googlemail.com>
2012-03-16 22:15:24 +0000

    Inital readme and allowed customer function to generate the retina image url

---
+++ README.md
@@ -0,0 +1,65 @@
+What is retina-replace.js?
+==========================
+
+- retinaReplace.js is a jQuery plugin for replacing out css background-images and img tag src's for a high quality retina version
+- Only runs on devices with a devicePixelRatio > 1
+- Preloads images before updating to eliviate flashing upon image update
+
+By default ``_2x`` is appended to the image url so ``/img/foo.jpg`` would become ``/img/foo_2x.jpg``
+both the suffix and retina url generating function can be overriden when inatalizing
+once an element's image has been replaced with its retina version the attribute ``data-retina`` is set to ``complete`` for example ``<img src="/img/foo.png" data-retina="complete" />``
+
+
+Using retina-replace.js
+-----------------------
+
+Simply call the retinaReplace function on the targeted elements
+
+```
+$('#foo, #bar').retinaReplace();
+```
+
+if using retina-replace.js to replace background images on elements then the add the following css to your stylesheet
+
+```
+[data-retina=complete] {
+    -webkit-background-size: 100%;
+    background-size:100%;
+}
+```
+
+
+Options
+-------
+
+<table>
+    <tr>
+        <td>Name</td>
+        <td>Type</td>
+        <td>Default</td>
+        <td>Descritpion</td>
+    </tr>
+    <tr>
+        <td>suffix</td>
+        <td>string</td>
+        <td>_2x</td>
+        <td>The default string to append to the current image url</td>
+    </tr>
+    <tr>
+        <td>generateUrl</td>
+        <td>function</td>
+        <td>appens the suffix value to the current url</td>
+        <td>The method used to generate the url for the retina image. Retrives two arguments, the current `element` and `url` determined from ether the image src (if an image tag) or background-image on all other elemnts</td>
+    </tr>
+</table>
+
+
+Markup
+------
+
+To quickly mark a element to be replaced just add ``data-retina="true"`` and any image source or background image will be replaced on retina enabled devices
+
+```
+<img src="/img/foo.jpg" data-retina="true" width="40" height="30">
+<div class="foo-with-background-image" data-retina="true"></div>
+```
--- retina-repalce.js
+++ retina-repalce.js
@@ -1,4 +1,23 @@
-(function($) {
+/* ============================================================
+ * retina-replace.js v1.0
+ * http://github.com/leonsmith/retina-replace-js
+ * ============================================================
+ * Author: Leon Smith
+ * Twitter: @nullUK
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ */
+ (function($) {
     "use strict";
     
     var retinaReplace = function(element, options) {
@@ -6,13 +25,10 @@
         this.options = options;
         var $el = $(element);
         var is_image = $el.is('img');
-        var src = is_image ? $el.attr('src') : $el.backgroundImageUrl();        
-        var dot_index = src.lastIndexOf('.');
-        var extension = src.substr(dot_index + 1);
-        var file = src.substr(0, dot_index);
-        var image_2x = file + this.options.suffix + '.' + extension;
+        var normal_url = is_image ? $el.attr('src') : $el.backgroundImageUrl();
+        var retina_url = this.options.generateUrl($el, normal_url);
 
-        $('<img/>').attr('src', image_2x).load(function() {
+        $('<img/>').attr('src', retina_url).load(function() {
 
             if (is_image) {
                 $el.attr('src', $(this).attr('src'));
@@ -29,6 +45,9 @@
     }
 
     $.fn.retinaReplace = function(option) {
+        // Finish if we arn't a retina device
+        if (getDevicePixelRatio() <= 1) { return this; }
+
         return this.each(function() {
             var $this = $(this);
             var data = $this.data('retinaReplace');
@@ -38,18 +57,31 @@
         });
     }
     
+    $.fn.retinaReplace.defaults = {
+        suffix: '_2x', 
+        generateUrl: function(element, url) {
+            var dot_index = url.lastIndexOf('.');
+            var extension = url.substr(dot_index + 1);
+            var file = url.substr(0, dot_index);
+            return file + this.suffix + '.' + extension;
+        }
+    }
+
+    $.fn.retinaReplace.Constructor = retinaReplace;
+
+    // Helper Functions
+    var getDevicePixelRatio = function() {
+        if (window.devicePixelRatio === undefined) { return 1; }
+        return window.devicePixelRatio;
+    }
+
     $.fn.backgroundImageUrl = function(url) {
         return url ? this.each(function () { 
             $(this).css("background-image", 'url("' + url + '")')
         }) : $(this).css("background-image").replace(/url\(|\)|"|'/g, "");
     }
 
-    $.fn.retinaReplace.defaults = {
-        suffix: '_2x'
-    }
-
-    $.fn.retinaReplace.Constructor = retinaReplace;
-
+    // Trigger replacement on elements that hav been marked up
     $(function(){
         $("[data-retina='true']").retinaReplace();
     });
